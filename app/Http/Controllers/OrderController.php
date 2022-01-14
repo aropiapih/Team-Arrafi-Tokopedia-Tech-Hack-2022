ba628e7dea9166246d67376faf1d4c4e7bfa3a8d
setya.wibawa <setya.wibawa@tokopedia.com>
2022-01-15 00:25:45 +0700

    order amount from frontend instead

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -23,18 +23,15 @@ class OrderController extends Controller
         try {
             /** @var OrderDetail[] $order_details */
             $order_details = [];
-            $amount = 0;
             foreach ($product_ids as $product_id) {
                 Cart::where('product_id', $product_id)->delete();
                 $order_detail = new OrderDetail();
                 $order_detail->product_id = $product_id;
                 $order_details[] = $order_detail;
-                $product = Product::find($product_id);
-                $amount += $product->price;
             }
 
             $order = new Order();
-            $order->amount = $amount;
+            $order->amount = $request->post('amount');
             $order->user_id = Auth::id();
             $order->save();
             $order->detail()->saveMany($order_details);
