7328d8d9aa64011929cf2488c49c297c2eae351a
Muhammad Arrafi <arrafi@tokopedia.com>
2022-01-14 14:56:50 +0700

    view structure layouts

---
+++ resources/views/shop-list.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('title') 
+Shopping List
+@endsection
+
+@section('prestyles') 
+
+@endsection
+
+@section('styles')
+
+@endsection
+
+@section('contents')
+
+@endsection
+
+
+@section('prescript')
+
+@endsection
+
+@section('script')
+
+@endsection
