c839d1fa16c42753c77fd561ca88e2b71af4ed44
Ahmad Syauqi <ahmadsyauqi1511@gmail.com>
2022-01-14 16:57:04 +0800

    migration table init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->char('gender', 1);
+            $table->string('no_hp');
+            $table->string('address');
+            $table->integer('shopping_limit');
+            $table->boolean('notif_limit');
             $table->rememberToken();
             $table->timestamps();
         });
