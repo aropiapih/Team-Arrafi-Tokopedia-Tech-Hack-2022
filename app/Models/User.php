791d5f91e4722c487a049530cb9efdeddac9d63e
setya.wibawa <setya.wibawa@tokopedia.com>
2022-01-15 03:27:36 +0700

    exceed limit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Carbon\Carbon;
 
+/**
+ * @property int $shopping_limit
+ * @property bool $notif_limit
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -48,35 +51,40 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function shoppingList() {
+    public function shoppingList()
+    {
         return $this->hasMany(ShoppingList::class);
     }
 
-    public function cart() {
+    public function cart()
+    {
         return $this->belongsToMany(Product::class, 'cart', 'user_id', 'product_id', 'id', 'id');
     }
 
-    public function order() {
+    public function order()
+    {
         return $this->hasMany(Order::class);
     }
 
-    public function spendThisMonth() {
+    public function spendThisMonth(): int
+    {
         return $this->hasMany(Order::class)
-        ->whereMonth('created_at', date('m'))
-        ->whereYear('created_at', date('Y'))
-        ->sum('amount');
+            ->whereMonth('created_at', date('m'))
+            ->whereYear('created_at', date('Y'))
+            ->sum('amount');
     }
 
-    public function orderPerMonth() {
+    public function orderPerMonth()
+    {
         $dateS = Carbon::now()->startOfMonth()->subMonth(6);
         $dateE = Carbon::now()->endOfMonth();
 
         return $this->hasMany(Order::class)
-        ->whereBetween('created_at', [$dateS,$dateE])
-        ->get()
-        ->sortBy('created_at')
-        ->groupBy(function($val) {
-            return Carbon::parse($val->created_at)->format('M');
-        });
+            ->whereBetween('created_at', [$dateS, $dateE])
+            ->get()
+            ->sortBy('created_at')
+            ->groupBy(function ($val) {
+                return Carbon::parse($val->created_at)->format('M');
+            });
     }
 }
